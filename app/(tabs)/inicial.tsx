86222a2b0fbd3d8f1c5f658ccbe6f4ef3fd868f9
um-gabriel <gn.nunes.07@gmail.com>
2025-02-21 21:44:24 -0300

    Iniciado Drawer e Tabs Navigation

---
+++ app/(tabs)/inicial.tsx
@@ -0,0 +1,10 @@
+import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+
+export default function Login() {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text style={{fontSize:30}}>Welcome to our app</Text>
+    </View>
+  )
+};
