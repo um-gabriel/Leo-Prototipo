37fe54dbba0be2c7748dee04794fc45c6f20de0d
unknown <leonardomachadoarroyo14@gmail.com>
2025-03-07 15:12:57 -0300

    07-03

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,20 +3,40 @@ import { Drawer } from 'expo-router/drawer';
 
 export default function Layout() {
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
+    <GestureHandlerRootView style={{ 
+      flex: 1 
+      }}>
       <Drawer>
 
         <Drawer.Screen 
             name="Home"
+            options={{
+              title:"Home",
+            }}
         />
         <Drawer.Screen 
             name="Avisos"
+            options={{
+              title:"Avisos",
+            }}
         />
         <Drawer.Screen 
-            name="Config"
+            name="Geral"
+            options={{
+              title:"Oportunidades em geral",
+            }}
         />
         <Drawer.Screen 
             name="CreateJob"
+            options={{
+              title:"Criar vagas ou serviços",
+            }}  
+        />
+        <Drawer.Screen 
+            name="Config"
+            options={{
+              title:"Configurações",
+            }}
         />
 
       </Drawer>
