3b3be5564cd9a835ffe2e4f33358ebf0f2cbfb94
unknown <leonardomachadoarroyo14@gmail.com>
2025-03-07 15:12:57 -0300

    07-03

---
+++ app/(tabs)/Geral.tsx
@@ -0,0 +1,10 @@
+import { View, Text, TextInput } from 'react-native';
+import React from 'react';
+
+export default function Geral() {
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text style={{fontSize:30}}>Tela de geral</Text>
+    </View>
+  )
+};
